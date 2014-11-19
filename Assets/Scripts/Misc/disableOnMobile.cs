592b7b5de49947fd57209d8f8453bf5ac9d97489
EverlessDrop41 <tomfperegrine@googlemail.com>
2014-11-19 17:41:16 +0000

    minor bug fixes

---
+++ Assets/Scripts/Misc/disableOnMobile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disableOnMobile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_ANDROID
+			this.gameObject.SetActive(false);
+		#endif
+	}
+}
